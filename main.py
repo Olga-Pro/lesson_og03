5099434ddf3bcfd7f6ce5a8b71c6ab65d87ca85c
Olga-Pro <oip_@inbox.ru>
2024-03-23 18:52:19 +0300

    add all const

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -11,7 +12,22 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Игра ТИР")
 
 # иконка
-icon = pygame.image.load("")
+icon = pygame.image.load("image/openmidj6_logo_shooting_gallery_cdbb2e40-f6bf-411f-b6ea-545a52ccd5a6.png")
+pygame.display.set_icon(icon)
+
+# цель - куда стрелять будем
+target_image =  pygame.image.load("image/alarm_clock_fotor.png")
+# размер изображения
+target_width = 50
+target_heigth = 50
+
+# случайные координаты для изображения
+target_x = random.randint(0, SCREEN_WIDTH - target_width)
+target_y = random.randint(0, SCREEN_HEIGHT - target_heigth)
+
+# цвет для заливки экрана
+color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255) )
+
 running = True
 while running:
     pass
