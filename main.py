eac950d4249c6483d283ecd31546947efac43d38
Olga-Pro <oip_@inbox.ru>
2024-03-23 18:03:18 +0300

    add const

--- main.py
+++ main.py
@@ -2,6 +2,16 @@ import pygame
 
 pygame.init()
 
+# размеры экрана
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+# название экрана
+pygame.display.set_caption("Игра ТИР")
+
+# иконка
+icon = pygame.image.load("")
 running = True
 while running:
     pass
